a31375967fc02c613a4e5b3866f2326970915a07
ostryzhniukn <nazarostryzhniuk@gmail.com>
2015-02-02 05:18:47 +0200

    restful reconfigured to run without spring-boot

--- if052-restful-service/src/main/java/com/softserveinc/if052_restful_service/controller/IndexController.java
+++ if052-restful-service/src/main/java/com/softserveinc/if052_restful_service/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class IndexController {
 
     private static final String TEMPLATE = "Hello, %s from REST! ";
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
     public HttpEntity<Greeting> greeting(
             @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
 
