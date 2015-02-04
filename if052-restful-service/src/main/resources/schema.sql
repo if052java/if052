19c586414698eb77355645016e5ebf2332c98d38
vblyfqn <vblyfqn@gmail.com>
2015-02-04 16:28:11 +0200

    ADDED ON CASCADE

--- if052-restful-service/src/main/resources/schema.sql
+++ if052-restful-service/src/main/resources/schema.sql
@@ -3,13 +3,13 @@
 
   CREATE TABLE address (address_id int(11) auto_increment primary key, city varchar(45), street varchar(45),
   building varchar(45), apartment varchar(45), user_id int(11),
-  CONSTRAINT fk_address_user FOREIGN KEY (user_id) REFERENCES user (user_id));
+  CONSTRAINT fk_address_user FOREIGN KEY (user_id) REFERENCES user (user_id) ON UPDATE CASCADE);
 
   CREATE TABLE watermeter(water_meter_id int(11) auto_increment primary key, name varchar(45), description varchar(45),
-  address_id int(11), CONSTRAINT fk_water_meter_adderess FOREIGN KEY (address_id) REFERENCES address (address_id));
+  address_id int(11), CONSTRAINT fk_water_meter_adderess FOREIGN KEY (address_id) REFERENCES address (address_id) ON UPDATE CASCADE);
 
   CREATE TABLE indicator(indicator_id int auto_increment primary key, date datetime, value int, is_paid bit(1),
   is_published bit(1), water_meter_id int(11),
-  CONSTRAINT fk_indicator_water_meter FOREIGN KEY (water_meter_id) REFERENCES watermeter (water_meter_id));
+  CONSTRAINT fk_indicator_water_meter FOREIGN KEY (water_meter_id) REFERENCES watermeter (water_meter_id) ON UPDATE CASCADE);
 
 
