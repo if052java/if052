6290aebea09d94a14061aa48bad85eb3cf56d0e4
ostryzhniukn <nazarostryzhniuk@gmail.com>
2015-02-06 15:27:13 +0200

    JSP support added; tomcat7-maven-plugin added; hello example changed.

--- if052-webapp/src/main/java/com/softserveinc/if052_webapp/controller/IndexController.java
+++ if052-webapp/src/main/java/com/softserveinc/if052_webapp/controller/IndexController.java
@@ -5,6 +5,7 @@ package com.softserveinc.if052_webapp.controller;
  */
 import com.softserveinc.if052_webapp.HelloWorld.Greeting;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,11 +21,13 @@ public class IndexController {
     }
 
     @RequestMapping("/greeting")
-    @ResponseBody
     public String greeting(@RequestParam(value = "name",
-            required = false, defaultValue = "World") String name){
+            required = false, defaultValue = "World") String name,
+                           ModelMap model){
         RestTemplate restTemplate = new RestTemplate();
         Greeting greeting = restTemplate.getForObject("http://localhost:8080/restful/greeting/"+name, Greeting.class);
-        return greeting.getContent();
+
+        model.addAttribute("message", greeting.getContent());
+        return "hello";
     }
 }
---
+++ if052-webapp/src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: student
+  Date: 2/6/2015
+  Time: 12:48 PM
+  To change this template use File | Settings | File Templates.
+  Author: Nazar Ostryzhniuk
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>JSP Hello</title>
+</head>
+<body>
+  <h2>${message}</h2>
+</body>
+</html>
