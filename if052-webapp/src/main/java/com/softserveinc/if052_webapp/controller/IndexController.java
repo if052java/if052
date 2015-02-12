577dbfd2132d7d37885a174461dc0799781f18a4
valentyn <valentyn@virtuoso.com>
2015-02-12 11:54:35 +0200

    Update IndexController

--- if052-webapp/src/main/java/com/softserveinc/if052_webapp/controller/IndexController.java
+++ if052-webapp/src/main/java/com/softserveinc/if052_webapp/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.softserveinc.if052_webapp.controller;
 /**
  * Created by nazar on 1/28/15.
  */
-import com.softserveinc.if052_restful.domain.User;
 import com.softserveinc.if052_webapp.HelloWorld.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -24,7 +23,7 @@ public class IndexController {
             required = false, defaultValue = "World") String name,
                            ModelMap model){
         RestTemplate restTemplate = new RestTemplate();
-        Greeting greeting = restTemplate.getForObject("http://localhost:8080/greeting/"+name, Greeting.class);
+        Greeting greeting = restTemplate.getForObject("http://localhost:8080/restful/greeting/"+name, Greeting.class);
 
         model.addAttribute("message", greeting.getContent());
         return "hello";
