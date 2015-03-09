878d57d8175e39dce321d6e415443345328ef22e
ostryzhniukn <nazarostryzhniuk@gmail.com>
2015-03-09 13:56:25 +0200

    Simple security config runs< but without login page and so on.

---
+++ if052-restful-service/src/main/java/com/softserveinc/if052_restful/oauth/SecurityConfiguration.java
@@ -0,0 +1,60 @@
+package com.softserveinc.if052_restful.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/oauth/uncache_approvals", "/oauth/cache_approvals");
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+                 http
+            .authorizeRequests().antMatchers("/login").permitAll().and()
+            .authorizeRequests().antMatchers("/getRoles/**").hasRole("USER").and()
+//            .authorizeRequests()
+//                .anyRequest().hasRole("USER")
+//                .and()
+            .exceptionHandling()
+                .accessDeniedPage("/login/authorization_error")
+                .and()
+            // TODO: put CSRF protection back into this endpoint
+            .csrf()
+                .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize")).disable()
+            .logout()
+                .logoutSuccessUrl("/index")
+                .logoutUrl("/logout")
+                .and()
+            .formLogin()
+                    .usernameParameter("j_username")
+                    .passwordParameter("j_password")
+                    .failureUrl("/login?authentication_error=true")
+                    .loginPage("/login")
+                    .loginProcessingUrl("/login/do");
+        // @formatter:on
+    }
+}
