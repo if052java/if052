24b728969c30fdcda7a5cae632d330eb98727928
Nazar <nazarostryzhniuk@gmail.com>
2015-03-19 18:54:11 +0200

    anything changed

--- if052-webapp/src/main/java/com/softserveinc/if052_webapp/oauth/SecurityConfig.java
+++ if052-webapp/src/main/java/com/softserveinc/if052_webapp/oauth/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-//
+
 //@Configuration
 //@EnableWebSecurity
 //public class SecurityConfig extends WebSecurityConfigurerAdapter {
