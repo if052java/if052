6599119c2b0d351bb5503aec3a148e5c34525c69
Nazar <nazarostryzhniuk@gmail.com>
2015-03-26 20:23:40 +0200

    webapp: no-XML config used

---
+++ if052-webapp/src/main/java/com/softserveinc/if052_webapp/config/ServletInitializer.java
@@ -0,0 +1,56 @@
+package com.softserveinc.if052_webapp.config;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+/**
+ * @author Nazar Ostryzhniuk
+ * 
+ */
+public class ServletInitializer extends AbstractDispatcherServletInitializer {
+
+	@Override
+	protected WebApplicationContext createServletApplicationContext() {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(WebMvcConfig.class);
+		return context;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected WebApplicationContext createRootApplicationContext() {
+		return null;
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+        registerCharacterEncodingFilter(servletContext, "encodingFilter");
+		//registerProxyFilter(servletContext, "springSecurityFilterChain");
+		//registerProxyFilter(servletContext, "oauth2ClientContextFilter");
+	}
+
+	private void registerProxyFilter(ServletContext servletContext, String name) {
+		DelegatingFilterProxy filter = new DelegatingFilterProxy(name);
+		filter.setContextAttribute("org.springframework.web.servlet.FrameworkServlet.CONTEXT.dispatcher");
+		servletContext.addFilter(name, filter).addMappingForUrlPatterns(null, false, "/*");
+	}
+
+    private void registerCharacterEncodingFilter(ServletContext servletContext, String name){
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        servletContext.addFilter(name, encodingFilter).addMappingForUrlPatterns(null, false, "/*");
+    }
+
+}
---
+++ if052-webapp/src/main/java/com/softserveinc/if052_webapp/config/WebMvcConfig.java
@@ -0,0 +1,89 @@
+package com.softserveinc.if052_webapp.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.softserveinc.if052_webapp.errorHandler.CustomErrorResponseHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan(basePackages = {"com.softserveinc.if052_webapp"})
+@EnableWebMvc
+@PropertySource("classpath:rest.properties")
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    @Value("${restAddress}")
+    public String restUrl(String restUrl){
+        return restUrl;
+    }
+
+    @Bean
+    public TilesConfigurer tilesConfigurer(){
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions("/WEB-INF/tiles/tiles-definitions.xml");
+        return tilesConfigurer;
+    }
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	@Bean
+	public ContentNegotiatingViewResolver contentViewResolver() throws Exception {
+		ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
+		ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
+		contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
+		contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
+		contentViewResolver.setDefaultViews(Arrays.<View> asList(new MappingJacksonJsonView()));
+		return contentViewResolver;
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+    @Bean
+    RestTemplate restTemplate(){
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setErrorHandler(new CustomErrorResponseHandler());
+        return restTemplate;
+    }
+
+    @Bean
+    ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
+}
