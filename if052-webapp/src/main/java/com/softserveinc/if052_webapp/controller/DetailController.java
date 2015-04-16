f33394502ccde7cd193b5bfb7542ab1cb18c4d3f
Vynnyk Maksym <maksimvinnik@gmail.com>
2015-04-16 14:52:56 +0300

    fixed bug in index page

--- if052-webapp/src/main/java/com/softserveinc/if052_webapp/controller/DetailController.java
+++ if052-webapp/src/main/java/com/softserveinc/if052_webapp/controller/DetailController.java
@@ -27,7 +27,7 @@ public class DetailController {
     public static final String LAST_INDICATORS = "lastIndicators";
     public static final String LIMIT = "limit";
     public static final String UNTRACKED_DAYS = "untrackedDays";
-    public static final String LIST_IS_EMPTY = "listIsEmpty";
+    public static final String LIST_SIZE = "listSize";
 
     @Autowired
     @Qualifier("restUrl")
@@ -50,16 +50,15 @@ public class DetailController {
 
         List<Indicator> indicators = Arrays.asList(arrOfIndicators);
 
+        model.addAttribute(LIST_SIZE, indicators.size());
+
         if (indicators.size()!=0){
             Date lastTrackedDay = indicators.get(0).getDate();
             long subtractInDays = (new Date().getTime() - lastTrackedDay.getTime())/(1000*60*60*24);
             model.addAttribute(UNTRACKED_DAYS, subtractInDays);
             model.addAttribute(LAST_INDICATORS, indicators);
             model.addAttribute(LIMIT, COUNT);
-        } else {
-            model.addAttribute(LIST_IS_EMPTY, true);
         }
-
         return "index";
     }
 }
