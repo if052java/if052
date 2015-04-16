da45ad863b33b9fc29216c4e65d214c9216b2cdb
ostryzhniukn <nazarostryzhniuk@gmail.com>
2015-04-16 23:36:46 +0300

    WaterMeterResource methods secured

--- if052-restful-service/src/main/java/com/softserveinc/if052_restful/config/CustomPermissionEvaluator.java
+++ if052-restful-service/src/main/java/com/softserveinc/if052_restful/config/CustomPermissionEvaluator.java
@@ -1,7 +1,9 @@
 package com.softserveinc.if052_restful.config;
 
 import com.softserveinc.if052_core.domain.Address;
+import com.softserveinc.if052_core.domain.WaterMeter;
 import com.softserveinc.if052_restful.service.AddressService;
+import com.softserveinc.if052_restful.service.WaterMeterService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.PermissionEvaluator;
@@ -18,21 +20,33 @@ public class CustomPermissionEvaluator implements PermissionEvaluator {
     @Autowired
     AddressService addressService;
 
+    @Autowired
+    WaterMeterService waterMeterService;
+
     private static Logger LOGGER = Logger.getLogger(CustomPermissionEvaluator.class.getName());
 
     @Override
     public boolean hasPermission(Authentication authentication, Object target, Object permission) {
         LOGGER.debug("getName: " + authentication.getName() + " permission: " + permission);
 
-        if (permission.toString().equals("getAddress")){
+        if (permission.toString().equals("getAddress")) {
             Address address = (Address) target;
             return check(address.getUser().getUserId(), authentication);
-        }else if (permission.toString().equals("udAddress")){
-            Integer addressId = (Integer)target;
-            LOGGER.debug("hasPermission addressId " + addressId);
+        } else if (permission.toString().equals("udAddress")) {
+            Integer addressId = (Integer) target;
             Address address = addressService.getAddressById(addressId);
             return check(address.getUser().getUserId(), authentication);
+        } else
+
+        if (permission.toString().equals("getWaterMeter")) {
+            WaterMeter waterMeter = (WaterMeter) target;
+            return check(waterMeter.getAddress().getUser().getUserId(), authentication);
+        } else if (permission.toString().equals("udWaterMeter")) {
+            Integer waterMeterId = (Integer) target;
+            WaterMeter waterMeter = waterMeterService.getWaterMeterById(waterMeterId);
+            return check(waterMeter.getAddress().getUser().getUserId(), authentication);
         }
+
         return false;
     }
 
@@ -41,12 +55,12 @@ public class CustomPermissionEvaluator implements PermissionEvaluator {
         throw new UnsupportedOperationException();
     }
 
-    public boolean check(Integer userId, Authentication authentication){
+    public boolean check(Integer userId, Authentication authentication) {
         LOGGER.debug("checking userId: " + userId);
         if (Integer.toString(userId).equals(authentication.getName())) {
             LOGGER.debug("hasPermission result: true");
             return true;
-        }else{
+        } else {
             LOGGER.debug("hasPermission result: false");
             return false;
         }
